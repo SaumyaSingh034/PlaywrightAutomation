8e49e1297e56a2aedf9d113ce954c645a8a6ee11
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-16 17:02:01 +0530

    First Execution

--- playwright.config.js
+++ playwright.config.js
@@ -26,9 +26,10 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    browserName: 'chromium'
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  
   },
 
   /* Configure projects for major browsers */
--- tests/UiBasic.spec.js
+++ tests/UiBasic.spec.js
@@ -3,13 +3,14 @@ const {test} = require('@playwright/test');
 test('First Plyawright Test Case', async ({browser})=>
     {
         const context = await browser.newContext();
-        const page = (await context).newPage();
-        (await page).goto("https://rahulshettyacademy.com/loginpagePractise/");
+        const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
 
 });
 
 test('Page Playwright Test', async ({page})=> {
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 
-})
+    await page.goto("https://google.com");
+
+});
