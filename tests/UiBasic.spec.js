72238466c9b7c8a4af3ee16033a5ae56e5975988
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-16 16:48:33 +0530

    Spec File Writing

---
+++ tests/UiBasic.spec.js
@@ -0,0 +1,15 @@
+const {test} = require('@playwright/test');
+
+test('First Plyawright Test Case', async ({browser})=>
+    {
+        const context = await browser.newContext();
+        const page = (await context).newPage();
+        (await page).goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+
+});
+
+test('Page Playwright Test', async ({page})=> {
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+})
