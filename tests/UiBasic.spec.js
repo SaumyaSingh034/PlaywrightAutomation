0680b68be99ab7fe2e6fa64a7b18756d5aba68f6
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-17 15:28:15 +0530

    Assertions in Playwright

--- tests/UiBasic.spec.js
+++ tests/UiBasic.spec.js
@@ -6,6 +6,16 @@ test('First Plyawright Test Case', async ({browser})=>
         const page = await context.newPage();
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
         console.log(await page.title());
+        await page.locator('#username').fill("saumyaSingh");
+        await page.locator("[type='password']").fill("learning");
+        await page.locator("#signInBtn").click();
+        var errorText = await page.locator("[style*='block']").textContent();
+        console.log(errorText);
+
+        //assertion
+        await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+
 
 
 });
