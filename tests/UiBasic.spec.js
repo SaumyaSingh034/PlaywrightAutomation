bc673d3e31aa872278831e87b14d38662e1963a2
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-17 18:00:19 +0530

    Playwright

--- tests/UiBasic.spec.js
+++ tests/UiBasic.spec.js
@@ -1,19 +1,31 @@
 const {test, expect} = require('@playwright/test');
+const { error } = require('console');
 
 test('First Plyawright Test Case', async ({browser})=>
     {
         const context = await browser.newContext();
         const page = await context.newPage();
+        const userName = page.locator("#username");
+        const password = page.locator("[type='password']");
+        const signInBtn = page.locator("#signInBtn");
+        const errorLocator = page.locator("[style*='block']");
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
         console.log(await page.title());
-        await page.locator('#username').fill("saumyaSingh");
-        await page.locator("[type='password']").fill("learning");
-        await page.locator("#signInBtn").click();
-        var errorText = await page.locator("[style*='block']").textContent();
+        await userName.fill("saumyaSingh");
+        await password.fill("learning");
+        await signInBtn.click();
+        var errorText = await errorLocator.textContent();
         console.log(errorText);
 
         //assertion
+        
         await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+        console.log("**************************************")
+
+        await userName.fill("");
+        await userName.fill("rahulshettyacademy");
+        await signInBtn.click();
+        console.log(await page.title());
 
 
 
